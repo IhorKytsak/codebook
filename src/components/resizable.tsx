4c8413de8dab3f404b6e5d20b8538b5401f1fbfe
Ihor Kytsak <kitsak735@gmail.com>
2023-02-27 15:16:38 +0200

    adaptation of the editor when changing the size of the browser window

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 import './resizable.css';
 
@@ -9,9 +9,24 @@ interface REsizableProps {
 const Resizable: React.FC<REsizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
 
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(window.innerWidth * 0.75);
+
   useEffect(() => {
+    let timer: any;
     const listener = () => {
-      console.log(window.innerWidth, window.innerHeight);
+      if (timer) {
+        clearTimeout(timer);
+      }
+
+      timer = setTimeout(() => {
+        setInnerHeight(window.innerHeight);
+        setInnerWidth(window.innerWidth);
+        if (window.innerWidth * 0.75 < width) {
+          setWidth(window.innerWidth * 0.75);
+        }
+      }, 100);
     };
 
     window.addEventListener('resize', listener);
@@ -24,15 +39,18 @@ const Resizable: React.FC<REsizableProps> = ({ direction, children }) => {
   if (direction === 'horizontal') {
     resizableProps = {
       className: 'resize-horizontal',
-      minConstraints: [window.innerWidth * 0.2, Infinity],
-      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      minConstraints: [innerWidth * 0.2, Infinity],
+      maxConstraints: [innerWidth * 0.75, Infinity],
       height: Infinity,
-      width: window.innerWidth * 0.75,
+      width,
       resizeHandles: ['e'],
+      onResizeStop: (_event, data) => {
+        setWidth(data.size.width);
+      },
     };
   } else {
     resizableProps = {
-      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      maxConstraints: [Infinity, innerHeight * 0.9],
       minConstraints: [Infinity, 44],
       height: 300,
       width: Infinity,
