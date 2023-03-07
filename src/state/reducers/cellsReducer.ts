8a11e5a7d264bffe0670ec2efddbd7c9fc0beb16
Ihor Kytsak <kitsak735@gmail.com>
2023-03-07 12:28:40 +0200

    fixed immer issue, and tested redux store

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -25,24 +25,29 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       const { id, content } = action.payload;
 
       state.data[id].content = content;
-      return;
+
+      return state;
+
     case ActionType.DELETE_CELL:
       delete state.data[action.payload];
       state.order = state.order.filter((id) => id !== action.payload);
-      return;
+
+      return state;
+
     case ActionType.MOVE_CELL:
       const { direction } = action.payload;
       const index = state.order.findIndex((id) => id === action.payload.id);
       const targetIndex = direction === 'up' ? index - 1 : index + 1;
 
       if (targetIndex < 0 || targetIndex > state.order.length - 1) {
-        return;
+        return state;
       }
 
       state.order[index] = state.order[targetIndex];
       state.order[targetIndex] = action.payload.id;
 
-      return;
+      return state;
+
     case ActionType.INSERT_CELL_BEFORE:
       const cell: Cell = {
         content: '',
@@ -62,10 +67,11 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
         state.order.splice(foundIndex, 0, cell.id);
       }
       return;
+
     default:
       return state;
   }
-});
+}, initialState);
 
 const randomId = () => {
   return Math.random().toString(36).substring(2, 8);
