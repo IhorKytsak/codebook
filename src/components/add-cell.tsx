b608633a0c4333b17f1d6771d4945d4d1eaf2da4
Ihor Kytsak <kitsak735@gmail.com>
2023-03-14 20:53:18 +0200

    add-cell component + refactoring insert action

---
+++ src/components/add-cell.tsx
@@ -0,0 +1,46 @@
+import { VscAdd } from 'react-icons/vsc';
+import { useActions } from '../hooks/useActions';
+import './add-cell.css';
+
+interface AddCellProps {
+  previousCellId: string | null;
+  forceVisible?: boolean;
+}
+
+interface AddButtonProps {
+  onClick: () => void;
+  text: string;
+}
+
+const AddButton: React.FC<AddButtonProps> = ({ onClick, text }) => {
+  return (
+    <button className='button is-rounded is-primary is-small' onClick={onClick}>
+      <span className='icon is-small'>
+        <VscAdd size={16} />
+      </span>
+      <span>{text}</span>
+    </button>
+  );
+};
+
+const AddCell: React.FC<AddCellProps> = ({ previousCellId, forceVisible }) => {
+  const { insertCellAfter } = useActions();
+
+  return (
+    <div className={`add-cell ${forceVisible && 'force-visible'}`}>
+      <div className='add-buttons'>
+        <AddButton
+          onClick={() => insertCellAfter(previousCellId, 'code')}
+          text='Code'
+        />
+        <AddButton
+          onClick={() => insertCellAfter(previousCellId, 'text')}
+          text='Text'
+        />
+      </div>
+      <div className='divider'></div>
+    </div>
+  );
+};
+
+export default AddCell;
